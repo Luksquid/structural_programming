267444410a5760c963eec8a8a911c6960427c4e7
LukasKalamarski <58850197+LukasKalamarski@users.noreply.github.com>
2021-03-01 13:51:25 +0100

    Create 1.4.11.c

---
+++ 1.4.11.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    int a, b, c, d; int x = 0;
+    scanf("%i %i %i %i", &a, &b, &c, &d);
+    // a)
+    do
+    {
+        x++;
+    }
+    while(fabs(a)*(x*x)+b*x+c<d);
+    printf("%i\n", x);
+    x=0;
+
+    // b)
+    do
+    {
+        x++;
+    }
+    while(5 *(x*x)+a*x+b>c);
+    printf("%i\n", x);
+    x=0;
+
+    //c
+    do
+    {
+        x++;
+    }
+    while(5 *(x*x)+a*x+b>=c);
+    printf("%i", x);
+}
