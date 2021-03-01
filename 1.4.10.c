ee8141fa8a225290fb9b3b6933424f2f9d19fb28
LukasKalamarski <58850197+LukasKalamarski@users.noreply.github.com>
2021-03-01 13:38:01 +0100

    Create 1.4.10.c

---
+++ 1.4.10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, wyn=1;
+    scanf("%i", &a);
+    for(int i = 1; i<a; i++)
+    {
+        if(i*i==a)
+        {
+            wyn=i;
+        }
+    }
+    printf("%i", wyn);
+}
