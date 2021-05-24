62ab1db7aaf250e8d949e6033a161a62daa9c4a7
LukasKalamarski <58850197+LukasKalamarski@users.noreply.github.com>
2021-05-24 20:02:06 +0200

    Update 5.2.22.c

--- 5.2.22.c
+++ 5.2.22.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <wchar.h>
-
+#include <string.h>
 
 void wys(char *nap)
 {
@@ -24,7 +24,7 @@ void wys2(wchar_t *nap)
 int ile(char *nap)
 {
     int temp = 0;
-    while(*(nap++))
+    while(nap[temp]!='\0')
     {
         temp++;
     }
@@ -41,53 +41,28 @@ int ile2(wchar_t *nap)
 }
 char *sklej(char *nap1, char *nap2, char *nap3)
 {
-    int i1 = ile(&*nap1);
-    int i2 = ile(&*nap2);
-    int i3 = ile(&*nap3);
-    char * napw = malloc((i3+i2+i1) * sizeof(char));
-    int t = 0;
-    for(int i=0; *(nap1+i)!=0; i++)
-    {
-        *(napw + t) = *(nap1+i);
-        t++;
-    }
-    for(int i=0; *(nap2+i)!=0; i++)
-    {
-        *(napw + t) = *(nap2+i);
-        t++;
-    }
-    for(int i=0; *(nap3+i)!=0; i++)
-    {
-        *(napw + t) = *(nap3+i);
-        t++;
-    }
-    napw[t]='\0';
-    return napw;
+    int i1 = ile(nap1);
+    int i2 = ile(nap2);
+    int i3 = ile(nap3);
+    char * wyn = malloc((i3+i2+i1+1) * sizeof(char));
+    wyn[0]='\0';
+    strcat(wyn, nap1);
+    strcat(wyn, nap2);
+    strcat(wyn, nap3);
+    return wyn;
 }
-wchar_t *sklej2(wchar_t *nap1, wchar_t *nap2, wchar_t *nap3)
+
+char *sklej2(wchar_t *nap1, wchar_t *nap2, wchar_t *nap3)
 {
-    int i1 = ile(&*nap1);
-    int i2 = ile(&*nap2);
-    int i3 = ile(&*nap3);
-    wchar_t * napw = malloc((i3+i2+i1) * sizeof(wchar_t));
-    int t = 0;
-    for(int i=0; *(nap1+i)!=0; i++)
-    {
-        *(napw + t) = *(nap1+i);
-        t++;
-    }
-    for(int i=0; *(nap2+i)!=0; i++)
-    {
-        *(napw + t) = *(nap2+i);
-        t++;
-    }
-    for(int i=0; *(nap3+i)!=0; i++)
-    {
-        *(napw + t) = *(nap3+i);
-        t++;
-    }
-    napw[t]='\0';
-    return napw;
+    int i1 = ile2(nap1);
+    int i2 = ile2(nap2);
+    int i3 = ile2(nap3);
+    wchar_t * wyn = malloc((i3+i2+i1+1) * sizeof(wchar_t));
+    wyn[0]='\0';
+    wcscat(wyn, nap1);
+    wcscat(wyn, nap2);
+    wcscat(wyn, nap3);
+    return wyn;
 }
 int main()
 {
@@ -97,9 +72,9 @@ int main()
     char * wyn = (sklej(&nap1, &nap2, &nap3));
     wys(wyn);
     printf("\n");
-    wchar_t nap4[] = L"Ala ";
-    wchar_t nap5[] = L"ma ";
-    wchar_t nap6[] = L"kota";
+    wchar_t nap4[] = L"U";
+    wchar_t nap5[] = L"W";
+    wchar_t nap6[] = L"M";
     wchar_t * wyn2 = (sklej2(&nap4, &nap5, &nap6));
     wys2(wyn2);
 
