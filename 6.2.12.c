ac92ef0558cc0be5430fa55270d8cadc04efbf70
LukasKalamarski <58850197+LukasKalamarski@users.noreply.github.com>
2021-05-24 19:34:55 +0200

    Update 6.2.12.c

--- 6.2.12.c
+++ 6.2.12.c
@@ -1,35 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <wchar.h>
-int foo(int** tab,int n, int m)
+void foo(char *nap1, char *nap2)
 {
-    int wyn = 0;
-    for(int i=0; i<n; i++)
+    int l = 0;
+    for(int i=0; nap1[i]!=0; i++)
     {
-        for(int j = 0; j<m; j++)
+        for(int j=0; nap2[j]!=0; j++)
         {
-            wyn += *(*(tab + i)+j);
+            if(nap1[i]==nap2[j])
+            {
+                l++;
+                break;
+            }
         }
     }
-    return wyn;
-
+    char * temp = malloc((1+l)*sizeof(char));
+    temp[l+1]='\0';
+    int t = 0;
+    for(int i=0; nap1[i]!=0; i++)
+    {
+        for(int j=0; nap2[j]!=0; j++)
+        {
+            if(nap1[i]==nap2[j])
+            {
+                temp[t]=nap1[i];
+                t++;
+                break;
+            }
+        }
+    }
+    t=0;
+    while(temp[t]!=0)
+    {
+        nap1[t] = temp[t];
+        t++;
+    }
+    nap1[t]='\0';
+}
+void foo2(wchar_t *nap1, wchar_t *nap2)
+{
+    int l = 0;
+    for(int i=0; nap1[i]!=0; i++)
+    {
+        for(int j=0; nap2[j]!=0; j++)
+        {
+            if(nap1[i]==nap2[j])
+            {
+                l++;
+                break;
+            }
+        }
+    }
+    wchar_t * temp = malloc((1+l)*sizeof(wchar_t));
+    temp[l]='\0';
+    int t = 0;
+    for(int i=0; nap1[i]!=0; i++)
+    {
+        for(int j=0; nap2[j]!=0; j++)
+        {
+            if(nap1[i]==nap2[j])
+            {
+                temp[t]=nap1[i];
+                t++;
+                break;
+            }
+        }
+    }
+    t=0;
+    while(temp[t]!=0)
+    {
+        nap1[t] = temp[t];
+        t++;
+    }
+    nap1[t]='\0';
+}
+void wys(char *nap)
+{
+    int temp = 0;
+    while(nap[temp]!=0)
+    {
+        printf("%c", nap[temp]);
+        temp++;
+    }
+}
+void wys2(wchar_t *nap)
+{
+    int temp = 0;
+    while(nap[temp]!=0)
+    {
+        wprintf(L"%c", nap[temp]);
+        temp++;
+    }
+}
 int main()
 {
-    int n = 5;
-    int m = 4;
-    int ** tab = malloc(n * sizeof(int**));
-    for(int i=0; i<n; i++)
-    {
-        *(tab + i) = malloc(m * sizeof(int*));
-    }
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<m; j++)
-        {
-            *(*(tab + i) + j) = i + j;
-            printf("[%d, %d] = %d\n", i, j, tab[i][j]);
-        }
-    }
-    printf("%d", foo(tab, n, m));
+    char nap1[] = "Ala ma kota";
+    char nap2[] = "Am t";
+    wchar_t nap3[] = L"Ala ma kota";
+    wchar_t nap4[] = L"alo";
+    foo(&nap1, &nap2);
+    wys(nap1);
+    printf("\n");
+    foo2(&nap3, &nap4);
+    wys2(nap3);
     return 0;
 }
